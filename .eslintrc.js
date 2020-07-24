7204b8ef43290423b4da070b6b6ad003a2bc2036
Dima <swatdimonbil@gmail.com>
2020-07-24 14:44:46 +0300

    project setup

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'linebreak-style': ["error", "windows"],
+    semi: ['error', 'always'],
+  },
+};
